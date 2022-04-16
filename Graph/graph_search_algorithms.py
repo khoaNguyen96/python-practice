8238a744c230085d6851b0802b49b71ff1b0ab41
KhoaNguyen96 <rin@192-168-1-9.tpgi.com.au>
2022-04-16 12:04:47 +1000

    Graph search algorithm

---
+++ Graph/graph_search_algorithms.py
@@ -0,0 +1,29 @@
+# depth-first search, using recursion and a stack, which is good to find out if a path is existed
+def dfs(graph, current_vertex, target_value, visited = None):
+    if visited is None:
+        visited = []
+    visited.append(current_vertex)
+    if current_vertex is target_value:
+        return visited
+  
+    for neighbor in graph[current_vertex]:
+        if neighbor not in visited:
+            path = dfs(graph, neighbor, target_value, visited)
+        if path:
+            return path
+    
+# breadth-first search, using a queue, which is good for search the shortest path 
+def bfs(graph, start_vertex, target_value):
+    path = [start_vertex]
+    vertex_and_path = [start_vertex, path]
+    bfs_queue = [vertex_and_path]
+    visited = set()
+    while bfs_queue:
+        current_vertex, path = bfs_queue.pop(0)
+        visited.add(current_vertex)
+        for neighbor in graph[current_vertex]:
+            if neighbor not in visited:
+                if neighbor is target_value:
+                    return path + [neighbor]
+                else:
+                    bfs_queue.append([neighbor, path + [neighbor]])
